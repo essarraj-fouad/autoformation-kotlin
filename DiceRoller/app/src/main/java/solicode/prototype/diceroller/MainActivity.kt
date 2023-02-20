04a1552d68919446bd11a6853f061702651fcc90
= <=>
2023-02-20 10:43:04 +0100

    Ajouter des images à l'application Dice Roller

--- DiceRoller/app/src/main/java/solicode/prototype/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/solicode/prototype/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package solicode.prototype.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
             rollDice()
 
         }
+        rollDice()
     }
 
     /**
@@ -22,8 +23,17 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        when(diceRoll){
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
